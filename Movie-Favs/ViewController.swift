b02893c381f935466d49d6bea7857be75ce1b336
Tim Knowles <tim@yourmarketingspecialist.com.au>
2016-03-15 15:16:35 +1100

    Initial Commit

---
+++ Movie-Favs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movie-Favs
+//
+//  Created by Tim Knowles on 15/03/2016.
+//  Copyright © 2016 Tim Knowles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
