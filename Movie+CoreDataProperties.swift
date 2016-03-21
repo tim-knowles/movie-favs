9e7f9ab01764a74fd061dc9df601d6102f576d0a
Tim Knowles <Tim@tims-macbook-air.local>
2016-03-21 13:45:06 +1100

    updated data model to remove the array type from Cast list

--- Movie+CoreDataProperties.swift
+++ Movie+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Movie+CoreDataProperties.swift
 //  Movie-Favs
 //
-//  Created by Tim Knowles on 15/03/2016.
+//  Created by Tim Knowles on 21/03/2016.
 //  Copyright © 2016 Tim Knowles. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -14,11 +14,11 @@ import CoreData
 
 extension Movie {
 
-    @NSManaged var movieTitle: String?
+    @NSManaged var movieCast: String?
     @NSManaged var movieDesc: String?
+    @NSManaged var movieImage: NSData?
     @NSManaged var movieLink: String?
     @NSManaged var movieRating: NSNumber?
-    @NSManaged var movieImage: NSData?
-    @NSManaged var movieCast: NSObject?
+    @NSManaged var movieTitle: String?
 
 }
